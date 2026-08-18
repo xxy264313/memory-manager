86bfb7e0d7805583f3a442fcd10ac6aec7f8cb0f
xxy264313 <2754758883@qq.com>
2026-08-18 16:27:54 +0800

    补全所有核心功能代码，覆盖旧版本

--- src/main.c
+++ src/main.c
@@ -1,39 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// TODO: 后续在这里定义链表结构体
+#include "../include/memory_manager.h"
 
 int main() {
     int M, Q;
     scanf("%d %d", &M, &Q);
-    
-    // TODO: 在这里初始化内存（初始时只有一块空闲内存 [0, M-1]）
-    
+    init_memory(M);
+
     for (int i = 0; i < Q; i++) {
         char cmd[20];
         scanf("%s", cmd);
-        
+
         if (strcmp(cmd, "ALLOC") == 0) {
             char id[16];
             int size;
             scanf("%s %d", id, &size);
-            // TODO: 调用你后面写的分配函数
-            printf("ALLOCATED %s 0\n", id); // 临时占位，方便测试
+            alloc_block(id, size);
         } 
         else if (strcmp(cmd, "FREE") == 0) {
             char id[16];
             scanf("%s", id);
-            // TODO
+            free_block(id);
+        } 
+        else if (strcmp(cmd, "QUERY") == 0) {
+            char id[16];
+            scanf("%s", id);
+            query_block(id);
         } 
         else if (strcmp(cmd, "DUMP") == 0) {
-            // TODO
-            printf("ALLOCATED_BLOCKS 0\nFREE_BLOCKS 1\n0 %d\n", M);
+            dump_memory();
         } 
         else if (strcmp(cmd, "COMPACT") == 0) {
-            // TODO
-            printf("COMPACTED 0\n");
+            compact_memory();
         }
     }
+
+    // 防止内存泄漏
+    free_all_memory();
     return 0;
 }
